c8f03a8669e498b9bb60b01718c5b36ef4764467
krptg <krptg0@gmail.com>
2020-08-28 11:33:53 +0200

    initial commit

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo cp asound.state /var/lib/alsa/asound.state
+sudo alsa-force-reload
+alsactl init
+sudo alsactl store --file /var/lib/alsa/asound.state
+sudo alsa force-reload
+exit 0
